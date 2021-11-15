a77b790c2b2eb4928f0b0fcdae4a59d962ed4b05
Saara Leppis <saara@leppihalme.fi>
2021-11-15 13:46:09 +0200

    random number counter added

--- src/App.js
+++ src/App.js
@@ -3,18 +3,48 @@ import Circle from "./Circle";
 import "./App.css";
 import { circles } from "./circles";
 
+const getRandInteger = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min)
+};
+
 class App extends Component {
   //it doesnt have to be state, can e separated js file
   // we can remove 4 times <Circle/> 
   state = {
     score: 0,
+    current: 0,
   };
+  timer = undefined;
+  pace = 1500;
 
   clickHandler = () => {
     this.setState({
       score: this.state.score + 10,
     });
   };
+
+  nextCircle = () => {
+    let nextActive;
+    do {
+      nextActive = getRandInteger(1, 4)
+    }
+    while (nextActive === this.state.current);
+
+    this.setState({
+      current: nextActive,
+    });
+    this.pace *= 0.95;
+    this.timer = setTimeout(this.nextCircle, this.pace);
+    console.log("active crcle is ", this.state.current);
+  }
+
+  startHandler = () => {
+    this.nextCircle();
+
+  }
+  stopHandler = () => {
+    clearTimeout(this.timer);
+  }
 
   render() {
     return (
@@ -28,8 +58,8 @@ class App extends Component {
             <Circle key={c.id} color={c.color} id={c.id} click={this.clickHandler} />
           ))}
         </div>
-        <button>START</button>
-        <button>STOP</button>
+        <button onClick={this.startHandler}>START</button>
+        <button onClick={this.stopHandler}>STOP</button>
       </div>
     );
   }
