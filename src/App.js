6914fe4922cd668c31f00fcbe464c67a214bde23
Saara Leppis <saara@leppihalme.fi>
2021-11-15 13:20:38 +0200

    added layout and some functionality

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
-import React from "react";
-import './App.css';
+import React, { Component } from "react";
+import Circle from "./Circle";
+import "./App.css";
+import { circles } from "./circles";
 
-function App() {
-  return (
-    <div className="App">
-      <p>
-        Hello,
-      </p>
-    </div>
-  );
+class App extends Component {
+  //it doesnt have to be state, can e separated js file
+  // we can remove 4 times <Circle/> 
+  state = {
+    score: 0,
+  };
+
+  clickHandler = () => {
+    this.setState({
+      score: this.state.score + 10,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <div>
+          <h1>Speed Game</h1>
+          <p> Your score: {this.state.score}</p>
+        </div>
+        <div className="circles">
+          {circles.map((c) => (
+            <Circle key={c.id} color={c.color} id={c.id} click={this.clickHandler} />
+          ))}
+        </div>
+        <button>START</button>
+        <button>STOP</button>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/circles.js
@@ -0,0 +1,2 @@
+export const circles = [
+    { id: 1, color: "#c97dd8" }, { id: 2, color: "#ab0012" }, { id: 3, color: "#98fb98" }, { id: 4, color: "#8fcbe2" }]
